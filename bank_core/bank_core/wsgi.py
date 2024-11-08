4d0f00b62c5aae241a9216508402828c7052497f
elmyrza attokurov <elmyrza217@gmail.com>
2024-11-08 15:54:57 +0600

    исправлен wsgi.py

--- bank_core/bank_core/wsgi.py
+++ bank_core/bank_core/wsgi.py
@@ -3,8 +3,8 @@ import sys
 from django.core.wsgi import get_wsgi_application
 
 # Добавьте путь к корневой директории проекта
-sys.path.append('/home/moibank/moi_bank')  # Путь к директории проекта
-sys.path.append('/home/moibank/moi_bank/bank_core')  # Путь к директории с модулем bank_core
+sys.path.append('/home/MoiBank')  # Путь к директории проекта
+sys.path.append('/home/Moibank/bank_core')  # Путь к директории с модулем bank_core
 
 # Укажите настройки Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bank_core.settings')
